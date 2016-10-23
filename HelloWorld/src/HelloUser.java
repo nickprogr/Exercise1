efcd760f708096d284f6069f22c7b0bd5bd3bee4
Nikolas Promponas <nickprogr@gmail.com>
2016-10-23 12:53:36 +0200

    Add new class HelloUser.java

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Nikolas Promponas
+ *
+ */
+public class HelloUser {
+	
+	private String userName;
+
+	public HelloUser(String userName) {
+		this.userName = userName;
+	}
+	
+	public void greetUser() {
+		System.out.println("Hello " + this.userName + "!");
+	}
+}
